642322fbe7d6da17bcbcac738c31fb8f6aac49a9
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2025-05-22 20:35:14 -0300

    cricao do colecoes em destaque usando o componente section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,21 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import ProductCard from './components/ProductCard';
 import ProductListing from './components/ProductListing';
+import Section from './components/Section';
 
 const App = () => {
     return (
         <>
             <div className="">
                 <Header />
+                <Section title="Colecoes em destaque">
+                </Section>
+                <div className="flex flex-col gap-3 max-w-[338px]" >
+                    <img className="rounded" src="../../public/collection-1.png" alt="" />
+                    <img className="rounded" src="../../public/collection-2.png" alt="" />
+                    <img className="rounded" src="../../public/collection-3.png" alt="" />
+                </div>
                 <ProductListing className='flex flex-wrap gap-2'>
-                    
                 </ProductListing>
                 <Footer />
             </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,9 @@
 const Section = ({title, titleAlign, link, children}) => {
     return (
         <>
+            <h5 className="text-dark-2 text-2xl text-left">{title}</h5>
+            <a className="text-primary text-lg" href={link}></a>
+            {children}
         </>
     );
 }

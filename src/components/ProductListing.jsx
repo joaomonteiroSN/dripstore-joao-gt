9e5a01db37a7fdd73e411ff70a01d1a914331e8a
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2025-06-03 17:05:21 -0300

    mais ajustes na renderizacao de imagens

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import products from "/public/data";
+import products from "/data";
 import ProductCard from "./ProductCard";
 
 const ProductListing = ({ mobileShowItens, desktopShowitens, maxShowItens = 15 }) => {

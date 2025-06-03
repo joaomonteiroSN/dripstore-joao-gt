b07302cba5fd6dd5e6a58171be35ccb10c5f04ad
joaomonteiroSN <70484799+joaomonteiroSN@users.noreply.github.com>
2025-06-03 14:03:49 -0300

    arquivos dist

---
+++ dist/data.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+        id:1,
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto1.png",
+        price: 200,
+        priceDiscount: 149.9
+    },
+    {
+        id:2,
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    {
+        name: "K-Swiss V8 - Masculino",
+        image: "https://url.imagem/do/produto2.png",
+        price: 49.9
+    },
+    
+
+]
+
+export default products
